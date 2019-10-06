d2be2f9782a5e23bc1457753e84e5d2f9a280d2e
eretica <eretica.m@gmail.com>
2019-10-06 16:02:49 +0900

    firebase settings

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        どやああ
         <a
           className="App-link"
           href="https://reactjs.org"
